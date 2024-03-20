e45c358cc4000f43facfc917efd3c045f10bf98d
Wael <waelassaf121@gmail.com>
2024-03-20 23:21:08 +0300

    Added a bottom textless logo in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => (
         <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
             <div className="flex flex-col justify-start items-start gap-6">
                 <Image
-                    src="/logo.png"
+                    src="/bottom-logo.svg"
                     alt="logo"
                     width={300}
                     height={50}
