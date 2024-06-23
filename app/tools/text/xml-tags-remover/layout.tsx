03796ab383ae35a73b2efe756e4c35fa04727fd6
Wael <waelassaf121@gmail.com>
2024-06-23 18:55:46 +0300

    Create Tool: XML Tags Remover

---
+++ app/tools/text/xml-tags-remover/layout.tsx
@@ -0,0 +1,18 @@
+const toolName = "XML Tags Remover"
+import { getDesc } from "@/utils"
+import type { Metadata } from "next"
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: toolName,
+        description: await getDesc(toolName),
+    }
+}
+
+export default async function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return <>{children}</>
+}
