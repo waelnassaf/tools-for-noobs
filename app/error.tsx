6d57703ab22a66d528648aaaab332c8c8551290e
Wael <waelassaf121@gmail.com>
2024-06-26 18:24:33 +0300

    Reload Page button

--- app/error.tsx
+++ app/error.tsx
@@ -19,7 +19,7 @@ export default function GlobalError({
             <button
                 className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
                 onClick={
-                    // Attempt to recover by trying to re-render the route
+                    //Reload Page
                     () => window.location.reload()
                 }
             >
