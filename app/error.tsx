1a9f7a04fa46010745dffc2bdd662a2607973b22
Wael <waelassaf121@gmail.com>
2024-07-12 18:31:34 +0300

    Just to keep green going

--- app/error.tsx
+++ app/error.tsx
@@ -19,7 +19,7 @@ export default function GlobalError({
             <button
                 className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
                 onClick={
-                    //Reload Page
+                    //Reload Page (Refresh)
                     () => window.location.reload()
                 }
             >
