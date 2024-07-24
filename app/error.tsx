0ad4f9d036d8b279c3da0d7f91abc00e2c869ee0
Wael <waelassaf121@gmail.com>
2024-07-24 19:08:35 +0300

    Imprive error handling

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 export default function GlobalError({
     error,
@@ -13,15 +14,15 @@ export default function GlobalError({
         console.error(error)
     }, [error])
 
+    // Access the router hook
+    const router = useRouter()
+
     return (
         <main className="flex h-screen flex-col items-center justify-start mt-10">
             <h2 className="text-center">Something went wrong!</h2>
             <button
                 className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
-                onClick={
-                    //Reload Page (Refresh)
-                    () => window.location.reload()
-                }
+                onClick={() => router.refresh()}
             >
                 Try again
             </button>
