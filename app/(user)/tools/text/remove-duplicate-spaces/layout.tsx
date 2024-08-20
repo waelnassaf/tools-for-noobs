d5d8f2a8dc4f13ac35ffd99f76c323a760807d57
Wael <waelassaf121@gmail.com>
2024-08-20 16:33:52 +0300

    New Tool: Remove Duplicate Spaces

---
+++ app/(user)/tools/text/remove-duplicate-spaces/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const toolName = "Remove Duplicate Spaces"
+import { getDesc } from "@/utils"
+import type { Metadata } from "next"
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: `${toolName} Online`,
+        description: await getDesc(toolName),
+    }
+}
+
+export default async function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return <>{children}</>
+}
